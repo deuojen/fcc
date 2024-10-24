0cb112a003c3d751c64e24b357e0de53197a2457
deuojen <qnurix@hotmail.com>
2024-10-24 12:02:32 +0100

    Update number_guessing.sh

--- number_guessing_game/number_guessing.sh
+++ number_guessing_game/number_guessing.sh
@@ -14,11 +14,11 @@ GAMES_PLAYED=0
 if [[ -z $USERNAME_ID ]]
 then
   INSERT_USER_RESULT=$($PSQL "insert into users(username, best_guess, games_played) values('$USERNAME', $BEST_GUESS, $GAMES_PLAYED)")
-  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+  echo -e "Welcome, $USERNAME! It looks like this is your first time here."
 else
   BEST_GUESS=$($PSQL "SELECT best_guess FROM users WHERE user_id =$USERNAME_ID")
   GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE user_id =$USERNAME_ID")
-  echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GUESS guesses."
+  echo -e "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GUESS guesses."
 fi
 
 RANDOM_NUMBER=$(( RANDOM % 1000 + 1))
@@ -28,7 +28,7 @@ GAME_FINISHED=0
 GUESS_THE_NUMBER() {
   if [[ ! $1 ]]
   then
-    echo -e "\nGuess the secret number between 1 and 1000:"
+    echo -e "Guess the secret number between 1 and 1000:"
     read CURRENT_GUESS
     (( NUMBER_OF_GUESS++ ))
   fi
@@ -46,26 +46,26 @@ GUESS_THE_NUMBER() {
       else
         UPDATE_USER_RESULT=$($PSQL "update users set games_played=$GAMES_PLAYED where username='$USERNAME'")
       fi
-      echo -e "\nYou guessed it in $NUMBER_OF_GUESS tries. The secret number was $RANDOM_NUMBER. Nice job!"
+      echo -e "You guessed it in $NUMBER_OF_GUESS tries. The secret number was $RANDOM_NUMBER. Nice job!"
 
     elif [[ $CURRENT_GUESS -lt $RANDOM_NUMBER ]]
     then
       # random number is greater
-      echo -e "\nIt's higher than that, guess again:"
+      echo -e "It's higher than that, guess again:"
       read CURRENT_GUESS
       (( NUMBER_OF_GUESS++ ))
     elif [[ $CURRENT_GUESS -gt $RANDOM_NUMBER ]]
     then
       # random number is lesser 
-      echo -e "\nIt's lower than that, guess again:"
+      echo -e "It's lower than that, guess again:"
       read CURRENT_GUESS
       (( NUMBER_OF_GUESS++ ))
     fi
   else
     # not a number
-    echo -e "\nThat is not an integer, guess again:"
+    echo -e "That is not an integer, guess again:"
     read CURRENT_GUESS
-    # (( NUMBER_OF_GUESS++ ))
+    (( NUMBER_OF_GUESS++ ))
   fi
 }
 
